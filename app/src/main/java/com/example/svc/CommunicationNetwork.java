fb10e0323afa8d623baf1f22a554cbd17946e86f
tzilbrmn <tzilbrmn94@gmail.com>
2021-12-15 12:42:09 +0200

    test

--- app/src/main/java/com/example/svc/CommunicationNetwork.java
+++ app/src/main/java/com/example/svc/CommunicationNetwork.java
@@ -181,7 +181,7 @@ public class CommunicationNetwork extends Thread {
             while (canListen) {
                 addEncounter.Listen();
             }
-            timer.join();
+            timer.join(); //Test
             return false;
         }
         else
